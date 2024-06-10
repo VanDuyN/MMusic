80208eb6912926f0e2e96a6baa698f8ec166e067
VanDuyN <vanduyfe@gmail.com>
2024-06-10 15:17:37 +0700

    first commit

---
+++ android/app/src/main/java/huflit/edu/vn/mmusic/MainActivity.java
@@ -0,0 +1,6 @@
+package huflit.edu.vn.mmusic;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mmusic/common/color_extension.dart';
+import 'package:mmusic/view/main_tabview/splash_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Music MFA',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "Roboto",
+        scaffoldBackgroundColor: TColor.bg,
+        textTheme: Theme.of(context).textTheme.apply(
+          bodyColor: TColor.primaryText,
+          displayColor: TColor.primaryText,
+        ),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: TColor.primary),
+        useMaterial3: false,
+
+      ),
+      home: const SplashView(),
+    );
+  }
+}
+
