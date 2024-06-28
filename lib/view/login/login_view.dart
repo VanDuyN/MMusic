8800d7e142ff9ddd2258db4ae01000d47bc918bc
Dang Phuong <dangthiphuong0712@gmail.com>
2024-06-28 14:06:29 +0700

    login register UI

--- lib/view/login/login_view.dart
+++ lib/view/login/login_view.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:mmusic/common/color_extension.dart';
-
-class LoginView extends StatefulWidget{
+import 'package:mmusic/view/register/register_view.dart';
+class LoginView extends StatefulWidget {
   const LoginView({super.key});
   @override
   State<LoginView> createState() => _LoginViewState();
-
 }
 
-class _LoginViewState extends State<LoginView>{
+class _LoginViewState extends State<LoginView> {
   bool _isObscurePassword = true;
   bool isChecked = false;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -36,123 +36,129 @@ class _LoginViewState extends State<LoginView>{
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 40),
-              child: Column(
-                children: [
-                  _buildTextField("Email", Icons.email_outlined),
-                  const SizedBox(height:16),
-                  _buildPasswordTextField("Mật khẩu", _isObscurePassword, Icons.lock_outline),
-                  Row(
-                    children: [
-                      Container(
-                        height: 25,
-                        width: 20,
-                        margin: EdgeInsets.all(2),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: TColor.primary.withOpacity(0.1),
-                              spreadRadius: 1,
-                              blurRadius: 8,
-                              offset: const Offset(0, 1),
-                            ),
-                          ],
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    _buildTextField("Email", Icons.email_outlined),
+                    const SizedBox(height: 16),
+                    _buildPasswordTextField("Mật khẩu", _isObscurePassword, Icons.lock_outline),
+                    Row(
+                      children: [
+                        Container(
+                          height: 25,
+                          width: 20,
+                          margin:  EdgeInsets.all(2),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: TColor.primary.withOpacity(0.1),
+                                spreadRadius: 1,
+                                blurRadius: 8,
+                                offset: const Offset(0, 1),
+                              ),
+                            ],
+                          ),
+                          child: Checkbox(
+                            checkColor: TColor.primary.withOpacity(0.7),
+                            value: isChecked,
+                            onChanged: (bool? value) {
+                              setState(() {
+                                isChecked = value!;
+                              });
+                            },
+                          ),
                         ),
-                        child:Checkbox(
-                          checkColor: TColor.primary.withOpacity(0.7),
-                          value: isChecked,
-                          onChanged: (bool? value) {
-                            setState(() {
-                              isChecked = value!;
-                            });
-                          },
+                        Text(
+                          "Nhớ mật khẩu",
+                          style: TextStyle(
+                              color: TColor.lightGray.withOpacity(0.7),
+                              fontWeight: FontWeight.w500,
+                              fontSize: 12),
                         ),
-                      ),
-                      Text(
-                        "Nhớ mật khẩu",
-                        style: TextStyle(
-                            color: TColor.lightGray.withOpacity(0.7),
-                            fontWeight: FontWeight.w500,
-                            fontSize: 12),
-                      ),
-                      Expanded(
-                        child: Text(
-                          "Quên mật khẩu?",
-                          textAlign: TextAlign.end,
-                          style: TextStyle(fontSize: 12, color: TColor.primary.withOpacity(0.5)),
-                        ),
-                      ),
-
-                    ],
-
-                  ),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(0,20,0,5),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50.0), // Độ cong của góc
-                      border: Border.all(
-                        color: TColor.primary.withOpacity(0.5), // Màu viền và độ mờ của nó
-                        width: 0, // Độ dày của viền
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: TColor.primary.withOpacity(0.8), // Màu của shadow
-                          spreadRadius: 1, // Bán kính lan rộng của shadow
-                          blurRadius: 10, // Độ mờ của shadow
-                          offset: const Offset(0, 2), // Độ dịch chuyển của shadow theo chiều dọc và ngang
+                        Expanded(
+                          child: Text(
+                            "Quên mật khẩu?",
+                            textAlign: TextAlign.end,
+                            style: TextStyle(fontSize: 12, color: TColor.primary.withOpacity(0.5)),
+                          ),
                         ),
                       ],
                     ),
-                    child: ElevatedButton(
-
-                      onPressed: () {
-                        // Xử lý khi button được nhấn
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: TColor.primary, // Màu chữ của button khi được nhấn
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0), // Độ cong của góc
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 20, 0, 5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50.0), // Độ cong của góc
+                        border: Border.all(
+                          color: TColor.primary.withOpacity(0.5), // Màu viền và độ mờ của nó
+                          width: 0, // Độ dày của viền
                         ),
-                        elevation: 5,
-                        fixedSize: const Size(200, 50), // Kích thước cố định của button
+                        boxShadow: [
+                          BoxShadow(
+                            color: TColor.primary.withOpacity(0.8), // Màu của shadow
+                            spreadRadius: 1, // Bán kính lan rộng của shadow
+                            blurRadius: 10, // Độ mờ của shadow
+                            offset: const Offset(0, 2), // Độ dịch chuyển của shadow theo chiều dọc và ngang
+                          ),
+                        ],
                       ),
-
-                      child: const Text(
-                        'Đăng nhập',
-                        style: TextStyle(color: Colors.white, fontSize: 16), // Màu chữ của button
-                      ),
-                    ),
-                  ),//button dnhap
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Bạn chưa có tài khoản? ",
-                        style: TextStyle(fontSize: 12,
-                            color: TColor.lightGray.withOpacity(0.7)
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState?.validate() ?? false) {
+                            // Xử lý khi form hợp lệ và button được nhấn
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: TColor.primary, // Màu chữ của button khi được nhấn
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0), // Độ cong của góc
+                          ),
+                          elevation: 5,
+                          fixedSize: const Size(200, 50), // Kích thước cố định của button
+                        ),
+                        child: const Text(
+                          'Đăng nhập',
+                          style: TextStyle(color: Colors.white, fontSize: 16), // Màu chữ của button
                         ),
                       ),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: TColor.primary.withOpacity(0.1),
-                              spreadRadius: 1,
-                              blurRadius: 8,
-                              offset: const Offset(0, 1),
+                    ), //button dnhap
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Bạn chưa có tài khoản? ",
+                          style: TextStyle(fontSize: 12, color: TColor.lightGray.withOpacity(0.7)),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: TColor.primary.withOpacity(0.1),
+                                spreadRadius: 1,
+                                blurRadius: 8,
+                                offset: const Offset(0, 1),
+                              ),
+                            ],
+                          ),
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const RegisterView()),
+                              );
+                            },
+                            child: Text(
+                              " Đăng ký ngay",
+                              style: TextStyle(color: TColor.primary),
                             ),
-                          ],
-                        ),
-
-                        child: GestureDetector(
-                          onTap:(){
-                          },
-                          child: Text(" Đăng ký ngay",style: TextStyle(color:TColor.primary ),),
-                        ),
-                      )
-                    ],
-                  ) // bạn chưa có tai khoan
-                ],
+                          ),
+                        )
+                      ],
+                    ) // bạn chưa có tai khoan
+                  ],
+                ),
               ),
             ),
           ],
@@ -163,7 +169,7 @@ class _LoginViewState extends State<LoginView>{
 
   Widget _buildTextField(String labelText, IconData icon) {
     return Container(
-      padding: const EdgeInsets.fromLTRB(10,0,10,0),
+      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
       decoration: BoxDecoration(
         color: TColor.darkGray,
         borderRadius: BorderRadius.circular(8.0),
@@ -177,6 +183,12 @@ class _LoginViewState extends State<LoginView>{
           labelStyle: TextStyle(color: TColor.unfocused),
           icon: Icon(icon, color: TColor.lightGray.withOpacity(0.5)),
         ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Vui lòng nhập $labelText';
+          }
+          return null;
+        },
       ),
     );
   }
@@ -205,11 +217,17 @@ class _LoginViewState extends State<LoginView>{
             ),
             onPressed: () {
               setState(() {
-                  _isObscurePassword = !_isObscurePassword;
+                _isObscurePassword = !_isObscurePassword;
               });
             },
           ),
-        )
+        ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Vui lòng nhập $labelText';
+          }
+          return null;
+        },
       ),
     );
   }
--- lib/view/register/register_view.dart
+++ lib/view/register/register_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mmusic/common/color_extension.dart';
+import 'package:mmusic/view/login/login_view.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -11,7 +12,7 @@ class RegisterView extends StatefulWidget {
 class _RegisterViewState extends State<RegisterView> {
   bool _isObscurePassword = true;
   bool _isObscureConfirmPassword = true;
-
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -37,81 +38,86 @@ class _RegisterViewState extends State<RegisterView> {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 40),
-              child: Column(
-                children: [
-                  _buildTextField("Họ và tên", Icons.person_outline),
-                  const SizedBox(height: 16),
-                  _buildTextField("Email", Icons.email_outlined),
-                  const SizedBox(height: 16),
-                  _buildPasswordTextField("Mật khẩu", _isObscurePassword, Icons.lock_outline),
-                  const SizedBox(height: 16),
-                  _buildPasswordTextField("Xác nhận mật khẩu", _isObscureConfirmPassword, Icons.key_outlined),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(0,20,0,5),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50.0), // Độ cong của góc
-                      border: Border.all(
-                        color: TColor.primary.withOpacity(0.5), // Màu viền và độ mờ của nó
-                        width: 0, // Độ dày của viền
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: TColor.primary.withOpacity(0.8), // Màu của shadow
-                          spreadRadius: 1, // Bán kính lan rộng của shadow
-                          blurRadius: 10, // Độ mờ của shadow
-                          offset: const Offset(0, 2), // Độ dịch chuyển của shadow theo chiều dọc và ngang
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    _buildTextField("Họ và tên", Icons.person_outline),
+                    const SizedBox(height: 16),
+                    _buildTextField("Email", Icons.email_outlined),
+                    const SizedBox(height: 16),
+                    _buildPasswordTextField("Mật khẩu", _isObscurePassword, Icons.lock_outline),
+                    const SizedBox(height: 16),
+                    _buildPasswordTextField("Xác nhận mật khẩu", _isObscureConfirmPassword, Icons.key_outlined),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0,20,0,5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50.0), // Độ cong của góc
+                        border: Border.all(
+                          color: TColor.primary.withOpacity(0.5), // Màu viền và độ mờ của nó
+                          width: 0, // Độ dày của viền
                         ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: TColor.primary.withOpacity(0.8), // Màu của shadow
+                            spreadRadius: 1, // Bán kính lan rộng của shadow
+                            blurRadius: 10, // Độ mờ của shadow
+                            offset: const Offset(0, 2), // Độ dịch chuyển của shadow theo chiều dọc và ngang
+                          ),
+                        ],
+                      ),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState?.validate() ?? false) {
+                            // Xử lý khi form hợp lệ và button được nhấn
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: TColor.primary, // Màu chữ của button khi được nhấn
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0), // Độ cong của góc
+                          ),
+                          elevation: 5,
+                          fixedSize: const Size(200, 50), // Kích thước cố định của button
+                        ),
+                        child: const Text(
+                          'Đăng ký',
+                          style: TextStyle(color: Colors.white, fontSize: 16), // Màu chữ của button
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Bạn đã có tài khoản? ",
+                          style: TextStyle(fontSize: 12, color: TColor.lightGray.withOpacity(0.7)),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: TColor.primary.withOpacity(0.1),
+                                spreadRadius: 1,
+                                blurRadius: 8,
+                                offset: const Offset(0, 1),
+                              ),
+                            ],
+                          ),
+                          child: GestureDetector(
+                            onTap:(){
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const LoginView()),
+                              );
+                            },
+                            child: Text(" Đăng nhập ngay",style: TextStyle(color:TColor.primary ),),
+                          ),
+                        )
                       ],
-                    ),
-                    child: ElevatedButton(
-
-                      onPressed: () {
-                        // Xử lý khi button được nhấn
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: TColor.primary, // Màu chữ của button khi được nhấn
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0), // Độ cong của góc
-                        ),
-                        elevation: 5,
-                        fixedSize: const Size(200, 50), // Kích thước cố định của button
-                      ),
-
-                      child: const Text(
-                        'Đăng ký',
-                        style: TextStyle(color: Colors.white, fontSize: 16), // Màu chữ của button
-                      ),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    
-                    children: [
-                      const Text("Bạn đã có tài khoản? "),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: TColor.primary.withOpacity(0.1),
-                              spreadRadius: 1,
-                              blurRadius: 8,
-                              offset: const Offset(0, 1),
-                            ),
-                          ],
-                        ),
-
-                        child: GestureDetector(
-                          onTap:(){
-                          },
-                          child: Text(" Đăng nhập ngay",style: TextStyle(color:TColor.primary ),),
-                        ),
-                      )
-
-
-                    ],
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
             ),
           ],
@@ -135,7 +141,15 @@ class _RegisterViewState extends State<RegisterView> {
           labelText: labelText,
           labelStyle: TextStyle(color: TColor.unfocused),
           icon: Icon(icon, color: TColor.lightGray.withOpacity(0.5)),
+          errorStyle: TextStyle(color: Color(0xfff64040).withOpacity(0.3), fontSize: 12), // Tùy chỉnh style của error text
+          errorBorder: InputBorder.none,// Tùy chỉnh style của error border
         ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Vui lòng nhập $labelText';
+          }
+          return null;
+        },
       ),
     );
   }
@@ -173,6 +187,12 @@ class _RegisterViewState extends State<RegisterView> {
             },
           ),
         ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Vui lòng nhập $labelText';
+          }
+          return null;
+        },
       ),
     );
   }
